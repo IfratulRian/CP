7807a265d20df6af89220ef73efd194234174232
Ifratul Islam Rian <ifrat.rian0007@gmail.com>
2026-08-16 09:24:19 +0600

    CF 1933A

--- CF1933A.cpp
+++ CF1933A.cpp
@@ -13,31 +13,14 @@ int main()
     ll t;
     cin>>t;
     while(t--){
-        string s;
-        cin>>s;
-        ll num=stoi(s);
-        if(num%7==0){
-            cout<<s<<endl;
-            continue;
+        ll n;
+        cin>>n;
+        ll sum=0;
+        for(int i=0;i<n;i++){
+            ll x;
+            cin>>x;
+            sum+=abs(x);
         }
-        int f=0;
-        for(ll i=0;i<s.size();i++){
-            char cc=s[i];
-            for(char c='0';c<='9';c++){
-                if(i==0 && c=='0'){
-                    continue;
-                }
-                s[i]=c;
-                if(stoi(s)%7==0){
-                    cout<<s<<endl;
-                    f=1;
-                    break;
-                }
-            }
-            if(f){
-                break;
-            }
-            s[i]==cc;
-        }
+        cout<<sum<<endl;
     }
 }
