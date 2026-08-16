6f3a3c2d188d1a6945b23d30b0bc4eb99897e286
Ifratul Islam Rian <ifrat.rian0007@gmail.com>
2026-08-16 09:27:20 +0600

    Codechef 1

---
+++ CC1.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define endl "\n"
+
+int main() {
+    int t;
+    cin >> t;
+    while(t--){
+        int x;
+        cin >> x;
+        if(x >= 80)cout << "YES" << endl;
+        else cout << "NO" << endl;
+    }
+    return 0;
+}
