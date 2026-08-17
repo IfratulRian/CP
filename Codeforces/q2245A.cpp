705b7ace985182a11a36ebe7b3804d3e7904513e
Ifratul Islam Rian <ifrat.rian0007@gmail.com>
2026-08-17 10:59:09 +0600

    CF 2245A

---
+++ Codeforces/q2245A.cpp
@@ -0,0 +1,35 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define endl '\n'
+#define fast()                   \
+    ios::sync_with_stdio(false); \
+    cin.tie(nullptr);            \
+    cout.tie(nullptr);
+
+int main(){
+    fast();
+    int t;
+    cin>>t;
+    while(t--){
+        int n,k;
+        cin>>n>>k;
+        string s;
+        cin>>s;
+        if(2*k>n){
+            cout<<-1<<endl;
+            continue;
+        }
+        int ans=0;
+        for(int i=0;i<k;i++){
+            if(s[i]=='L'){
+                ans++;
+            }
+        }
+        for(int i=n-k;i<n;i++){
+            if(s[i]=='R'){
+                ans++;
+            }
+        }
+        cout<<ans<<endl;
+    }
+}
