8747fe539fb28f93d495cc005a680335b015424c
Ifratul Islam Rian <ifrat.rian0007@gmail.com>
2026-08-18 10:45:29 +0600

    cc3

---
+++ Codechef/CC3.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define endl '\n'
+#define fast()                   \
+    ios::sync_with_stdio(false); \
+    cin.tie(nullptr);            \
+    cout.tie(nullptr);
+
+int main()
+{
+    fast();
+    int t;
+    cin >> t;
+    while(t--){
+        int x;
+        cin >> x;          
+        cout << x - 10 << endl;
+    }
+}
