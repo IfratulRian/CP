7de8caa6f556b8b42cf7d64e9c15de0033ab7a2f
Ifratul Islam Rian <ifrat.rian0007@gmail.com>
2026-08-21 21:31:46 +0600

    Correct Leetcode 3069

--- Leetcode/Leetcode3069.cpp
+++ Leetcode/Leetcode3069.cpp
@@ -11,8 +11,10 @@ class Solution {
 public:
     vector<int> resultArray(vector<int>& nums) {
         vector<int>e,o,ans;
-        for(int i=0;i<nums.size();i++){
-            if(i%2==0)e.push_back(nums[i]);
+        e.push_back(nums[0]);
+        o.push_back(nums[1]);
+        for(int i=2;i<nums.size();i++){
+            if(e.back()>o.back())e.push_back(nums[i]);
             else o.push_back(nums[i]);
         }
         for(auto x:e)ans.push_back(x);
