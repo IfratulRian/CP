10e82a20e7343e0bb970c76e1d0040f3d994c91d
Ifratul Islam Rian <ifrat.rian0007@gmail.com>
2026-08-24 12:02:08 +0600

    LEETCODE 4030

--- Leetcode/LEETCODE4030.cpp
+++ Leetcode/LEETCODE4030.cpp
@@ -1,5 +1,6 @@
 #include <bits/stdc++.h>
 using namespace std;
+#define ll long long
 #define fast()                   \
     ios::sync_with_stdio(false); \
     cin.tie(nullptr);            \
@@ -8,9 +9,15 @@ using namespace std;
 class Solution {
 public:
     bool isPalindromic(string s) {
-        if(s.size()==1)return false;
-        for(int i=0,j=s.size()-1;i<j;i++,j--){
-            if(s[i]!=s[j]){
+        // if(s.size()==1)return false;
+        // int k=s;
+        string f="";
+        for(auto x:s){
+            int k=x;
+            f+=bitset<8>(k).to_string();
+        }
+        for(int i=0,j=f.size()-1;i<j;i++,j--){
+            if(f[i]!=f[j]){
                 return false;
             }
         }
