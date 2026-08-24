a6ceab55d712a8bfa9c2c2b6484bf4e2d8bbf69e
Ifratul Islam Rian <ifrat.rian0007@gmail.com>
2026-08-24 09:36:53 +0600

    CC9

---
+++ Codechef/CC9.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define endl '\n'
+#define fast()                   \
+    ios::sync_with_stdio(false); \
+    cin.tie(nullptr);            \
+    cout.tie(nullptr);
+
+int main()
+{
+    fast();
+    int t;
+    cin>>t;
+    while(t--){
+        int n;
+        cin>>n;
+        vector<int>v(2*n),ans;
+        map<int,int>freq;
+        for(auto &x:v)cin>>x;
+        for(int i=0;i<2*n;i++){
+            freq[v[i]]++;
+            if(freq[v[i]]==1)ans.push_back(v[i]);
+        }
+        for(auto x:ans)cout<<x<<' ';
+        cout<<endl;
+    }
+}
