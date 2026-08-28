22a540659f0b2f203dce78d90f8c572456569e47
Md. Ifratul Islam Rian <ifrat.rian0007@gmail.com>
2026-08-28 10:46:07 +0600

    Improve input processing and calculation in 2036B.cpp
    Refactor input handling and summation logic in main function.

--- Codeforces/2036B.cpp
+++ Codeforces/2036B.cpp
@@ -2,6 +2,7 @@
 
 using namespace std;
 #define ll long long
+#define endl "\n"
 
 int main() {
     ios::sync_with_stdio(false);
@@ -12,20 +13,20 @@ int main() {
         ll n,k;
         cin>>n>>k;
         //vector<pair<ll,ll>>v(k);
-        vector<ll>v(k),a(k);
+        // vector<ll>v(k),a(k);
+        vector<ll>sum(k+1,0);
         for(ll i=0;i<k;i++){
-            cin>>a[i]>>v[i];
+            ll b,c;
+            cin>>b>>c;
+            sum[b]+=c;
         }
         // sort(v.begin(), v.end(), [](const auto& a, const auto& b) {
         //     return a.second < b.second; 
         // });
-        sort(v.begin(),v.end());
-        reverse(v.begin(),v.end());
-        ll sum=0;
-        for(ll i=0;i<n && i<k;i++){
-            sum+=v[i];
-        }
-        cout<<sum<<endl;
+        sort(sum.rbegin(),sum.rend());
+        ll ans=0;
+        for(ll i=0;i<n&&i<=k;i++)ans+=sum[i];
+        cout<<ans<<endl;
     }
     return 0;
 }
