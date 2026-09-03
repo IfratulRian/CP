bdd88656f413c82d1f31599dc0764c63cb05fda6
Ifratul Islam Rian <ifrat.rian0007@gmail.com>
2026-09-03 20:44:05 +0600

    CC10

---
+++ Codechef/CC10.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define fast()                   \
+    ios::sync_with_stdio(false); \
+    cin.tie(nullptr);            \
+    cout.tie(nullptr);
+
+int main(){
+    fast();
+    int t;
+    cin>>t;
+    while(t--){
+        int x;
+	    cin>>x;
+	    if(x>30)cout<<"yes"<<endl;
+	    else cout<<"no"<<endl;
+    }
+}
