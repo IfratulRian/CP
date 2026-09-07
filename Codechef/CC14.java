9d03a8468a7ea835a062f5c1b66e8cfb6a5339e6
Md. Ifratul Islam Rian <ifrat.rian0007@gmail.com>
2026-09-07 11:02:22 +0600

    Add CC14.java for problem solution

---
+++ Codechef/CC14.java
@@ -0,0 +1,17 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class CC14
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc = new Scanner(System.in);
+		int t = sc.nextInt();
+		while(t-- >0){
+		    int n = sc.nextInt();
+		    if(n>24)System.out.println("Yes");
+		    else System.out.println("No");
+		}
+	}
+}
