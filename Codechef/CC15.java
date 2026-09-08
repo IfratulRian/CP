85a929d34173d660405797d8b2d227be0343e942
Md. Ifratul Islam Rian <ifrat.rian0007@gmail.com>
2026-09-08 10:35:06 +0600

    Add CC15.java for division of two integers

---
+++ Codechef/CC15.java
@@ -0,0 +1,19 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class CC15
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc = new Scanner(System.in);
+		int t = sc.nextInt();
+		while(t-- >0){
+		    int a,b;
+		    a = sc.nextInt();
+		    b = sc.nextInt();
+		    int div = b/a;
+		    System.out.println(div);
+		}
+	}
+}
