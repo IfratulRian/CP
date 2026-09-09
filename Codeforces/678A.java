21e48bf0bca0ccb7d95122024c67d1ebb3ddbc10
Md. Ifratul Islam Rian <ifrat.rian0007@gmail.com>
2026-09-09 12:05:10 +0600

    Add solution for Codeforces problem 678A

---
+++ Codeforces/678A.java
@@ -0,0 +1,20 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+public class CF678A
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc = new Scanner(System.in);
+		long n,k;
+		n=sc.nextLong();
+		k=sc.nextLong();
+		for(long i=n+1;;i++){
+		    if(i%k==0){
+		        System.out.println(i);
+		        break;
+		    }
+		}
+	}
+}
