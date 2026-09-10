81e26530e9d63b78d302e40780bb073e9edbf5b1
Md. Ifratul Islam Rian <ifrat.rian0007@gmail.com>
2026-09-10 12:07:23 +0600

    Rename class and refactor main method

--- Codeforces/1739A.java
+++ Codeforces/1739A.java
@@ -2,20 +2,19 @@ import java.util.*;
 import java.lang.*;
 import java.io.*;
 
-public class 1739A
+public class Codechef
 {
-    public static void main (String[] args) throws java.lang.Exception
-    {
-        Scanner sc = new Scanner(System.in);
-        int t = sc.nextInt();
-        while(t-- >0){
-            int n,m;
-            n=sc.nextInt();m=sc.nextInt();
-            if(n<=2 || m<=2)System.out.println(1+" "+1);
-            else if(n==3 && m==3)System.out.println(2+" "+2);
-            else if(n==3)System.out.println(2+" "+1);
-            else if(m==3)System.out.println(1+" "+2);
-            else System.out.println(1+" "+1);
-        }
-    }
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc = new Scanner(System.in);
+		int t = sc.nextInt();
+		while(t-->0){
+		    int n,m;
+		    n=sc.nextInt();m=sc.nextInt();
+		    if(n==3 && m==3)System.out.println(2+" "+2);
+		    else if(n==3)System.out.println(2+" "+1);
+		    else if(m==3)System.out.println(1+" "+2);
+		    else System.out.println(1+" "+1);
+		}
+	}
 }
