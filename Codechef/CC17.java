3b2df6e38de27df6013239163534fb142290f61a
Md. Ifratul Islam Rian <ifrat.rian0007@gmail.com>
2026-09-13 19:55:48 +0600

    Format Scanner initialization for consistency

--- Codechef/CC17.java
+++ Codechef/CC17.java
@@ -5,7 +5,7 @@ public class CC17
 {
 	public static void main (String[] args) throws java.lang.Exception
 	{
-		Scanner sc =new Scanner(System.in);
+		Scanner sc = new Scanner(System.in);
 		int t = sc.nextInt();
 		while(t-- >0)
 		{
