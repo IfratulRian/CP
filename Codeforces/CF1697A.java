4c9c5fc199346d547b4f4a4f1298c5bdad5f4a2c
Md. Ifratul Islam Rian <ifrat.rian0007@gmail.com>
2026-09-15 10:18:17 +0600

    Format Scanner initialization in CF1697A.java

--- Codeforces/CF1697A.java
+++ Codeforces/CF1697A.java
@@ -6,7 +6,7 @@ public class CF1697A
 {
 	public static void main (String[] args) throws java.lang.Exception
 	{
-		Scanner sc = new Scanner(System.in);
+		Scanner sc= new Scanner(System.in);
 		int t = sc.nextInt();
 		while(t-- >0){
 		    int n=sc.nextInt();
