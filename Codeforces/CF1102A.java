d02f5910be70b3c86db0f5db306618da8d926fa0
Md. Ifratul Islam Rian <ifrat.rian0007@gmail.com>
2026-09-16 09:38:44 +0600

    Add CF1102A.java for Codeforces problem solution

---
+++ Codeforces/CF1102A.java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+public class CF1102A {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        long n = sc.nextLong();
+        if(n%4==1 || n%4==2)System.out.println(1);
+        else System.out.println(0);
+    }
+}
