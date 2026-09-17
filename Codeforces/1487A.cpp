508925a7431b785386442b76184a446ddda915ff
Md. Ifratul Islam Rian <ifrat.rian0007@gmail.com>
2026-09-17 10:54:02 +0600

    Create 1487A.cpp

---
+++ Codeforces/1487A.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define endl '\n'
+#define fast()                   \
+    ios::sync_with_stdio(false); \
+    cin.tie(nullptr);            \
+    cout.tie(nullptr);
+
+int main() {
+	int t;
+	cin>>t;
+	while(t--){
+	    int n;
+	    cin>>n;
+	    vector<int>v(n);
+	    for(auto &x:v)cin>>x;
+	    int mn=*min_element(v.begin(),v.end());
+	    int ans=0;
+	    for(auto x:v)if(x>mn)ans++;
+	    cout<<ans<<endl;
+	}
+}
