5960114f592428e0f849038460d7ecd0bb2144ff
Md. Ifratul Islam Rian <ifrat.rian0007@gmail.com>
2026-09-18 11:26:11 +0600

    Update CF1886A.java

--- Codeforces/CF1886A.java
+++ Codeforces/CF1886A.java
@@ -17,7 +17,7 @@ public class CF1886A
             System.out.println("YES");
             if(n%3 == 0)System.out.println("2 5 "+(n-7));
             else if(n%3 == 1)System.out.println("1 4 "+(n-5));
-            else System.out.println("1 2 "+(n - 3));
+            else System.out.println("1 2 "+(n- 3));
 		}
 	}
 }
