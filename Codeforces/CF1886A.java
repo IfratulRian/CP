fa4c96a2f020b9b4ea2ae3a7fd82c30c5fd6a632
Md. Ifratul Islam Rian <ifrat.rian0007@gmail.com>
2026-09-18 11:23:39 +0600

    Create CF1886A.java

---
+++ Codeforces/CF1886A.java
@@ -0,0 +1,23 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+public class CF1886A
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc = new Scanner(System.in);
+		int t = sc.nextInt();
+		while(t-- >0){
+		    long n = sc.nextInt();
+		    if(n<7 || n==9){
+                System.out.println("NO");
+                continue;
+            }
+            System.out.println("YES");
+            if(n%3 == 0)System.out.println("2 5 " + (n - 7));
+            else if(n%3 == 1)System.out.println("1 4 " + (n - 5));
+            else System.out.println("1 2 " + (n - 3));
+		}
+	}
+}
