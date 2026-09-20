62daba5bcc932563c65a0812d2462823a5f098c1
Md. Ifratul Islam Rian <ifrat.rian0007@gmail.com>
2026-09-20 20:14:47 +0600

    Update 1913A.cpp

--- Codeforces/1913A.cpp
+++ Codeforces/1913A.cpp
@@ -9,7 +9,7 @@ int main(){
         cin >> s;
         int f = 0;
         for(int i = 1; i < s.size(); i++){
-            string x = s.substr(0,i);
+            string x = s.substr( 0,i);
             string y=s.substr(i);
             if(y[0]=='0')continue;
             long long a=stoll(x);
