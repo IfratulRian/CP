3e3aff3689d1459d62b10faab2c8f4ce3e982d6d
Md. Ifratul Islam Rian <ifrat.rian0007@gmail.com>
2026-09-20 10:33:33 +0600

    Add CC23.java for processing input values

---
+++ Codechef/CC23.java
@@ -0,0 +1,21 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class CC23
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		for(int i=1;i<=n;i++){
+		    int X=sc.nextInt();
+		    int Y=sc.nextInt();
+		    if(Y>X){
+		        System.out.println(Y-X);
+		    }
+		    else{
+		        System.out.println(0);
+		    }
+		}
+	}
+}
