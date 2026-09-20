b64451396697297041e63609fa329467c565ce25
Md. Ifratul Islam Rian <ifrat.rian0007@gmail.com>
2026-09-20 20:15:48 +0600

    Refactor substring handling in Codeforces 1913A

--- Codeforces/1913A.cpp
+++ Codeforces/1913A.cpp
@@ -10,11 +10,11 @@ int main(){
         int f = 0;
         for(int i = 1; i < s.size(); i++){
             string x = s.substr( 0, i);
-            string y =s.substr(i);
-            if(y[0]=='0')continue;
-            long long a=stoll(x);
-            long long b=stoll(y);
-            if(a<b){
+            string y = s.substr( i );
+            if(y[0] == '0') continue;
+            long long a = stoll( x );
+            long long b = stoll( y );
+            if(a < b){
                 cout<<a<<" "<<b<<"\n";
                 f=1;
                 break;
