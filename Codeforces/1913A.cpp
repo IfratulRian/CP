afc821d06433da44eb23d2f36853476c5231c907
Md. Ifratul Islam Rian <ifrat.rian0007@gmail.com>
2026-09-20 20:13:50 +0600

    Update 1913A.cpp

--- Codeforces/1913A.cpp
+++ Codeforces/1913A.cpp
@@ -8,7 +8,7 @@ int main(){
         string s;
         cin >> s;
         int f = 0;
-        for(int i = 1;i<s.size();i++){
+        for(int i = 1; i<s.size();i++){
             string x=s.substr(0,i);
             string y=s.substr(i);
             if(y[0]=='0')continue;
