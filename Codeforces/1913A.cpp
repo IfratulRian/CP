1e18c843d8c008701b7c89e7ff91adc57f6fa337
Md. Ifratul Islam Rian <ifrat.rian0007@gmail.com>
2026-09-20 20:13:04 +0600

    Update 1913A.cpp

--- Codeforces/1913A.cpp
+++ Codeforces/1913A.cpp
@@ -6,7 +6,7 @@ int main(){
     cin>>t;
     while(t--){
         string s;
-        cin >>s;
+        cin >> s;
         int f=0;
         for(int i=1;i<s.size();i++){
             string x=s.substr(0,i);
