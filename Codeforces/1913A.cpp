279ba0627676958afcd28db72d3c1aa4e79d52a5
Md. Ifratul Islam Rian <ifrat.rian0007@gmail.com>
2026-09-20 10:31:53 +0600

    Create 1913A.cpp

---
+++ Codeforces/1913A.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        string s;
+        cin>>s;
+        int f=0;
+        for(int i=1;i<s.size();i++){
+            string x=s.substr(0,i);
+            string y=s.substr(i);
+            if(y[0]=='0')continue;
+            long long a=stoll(x);
+            long long b=stoll(y);
+            if(a<b){
+                cout<<a<<" "<<b<<"\n";
+                f=1;
+                break;
+            }
+        }
+        if(!f)cout<<-1<<"\n";
+    }
+}
